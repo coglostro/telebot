ba9f89430f864e0a8a7a26cd3917d52801f1c709
root <root@localhost.localdomain>
2020-05-06 21:53:59 +0200

    Cats and keyboard

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+from emoji import emojize
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
+from random import choice
 import logging
 import settings
 
@@ -8,17 +12,56 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     )
 
 
-def greet_user(update, bot):
-    text = 'start'
-    print(text)
-    bot.bot.send_message(chat_id=update.effective_chat.id, text=text)
+def greet_user(update, context):
+    emo = get_user_emo(context.user_data)
+    context.user_data['emo'] = emo
+    user_text = f'Привет {emo}'
+    contact_button = KeyboardButton('Контактные данные', request_contact=True)
+    location_button = KeyboardButton('Геолокация', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([
+                                        ['Прислать котика', 'Сменить аватар'],
+                                        [contact_button, location_button]
+                                        ],
+                                         True)
 
-def talk_to_me(update, bot):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=user_text, reply_markup=my_keyboard)
 
-    user_text = f'Привет {update.message.chat.first_name} Ты написал: {update.message.text}'
-    print(user_text)
-    logging.info("Привет %s, Chat id: %s, Message: %s", update.message.chat.first_name, update.message.chat.id, update.message.text)
-    bot.bot.send_message(chat_id=update.effective_chat.id, text=user_text)
+
+def talk_to_me(update, context):
+    emo = get_user_emo(context.user_data)
+    user_text = f'Привет {update.message.chat.first_name} {emo} Ты написал: {update.message.text}'
+    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.first_name, update.message.chat.id, update.message.text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=user_text)
+
+
+def send_cat_picture(update, context):
+    cat_list = glob('images/cat*.jp*g')
+    cat_pic = choice(cat_list)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(cat_pic, 'rb'))
+
+def chenge_avatar(update, context):
+    if 'emo' in context.user_data:
+        del context.user_data['emo']
+    emo = get_user_emo(context.user_data)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Готово {emo}')
+
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        return emojize(choice(settings.USER_EMOJI), use_aliases=True)
+
+def get_contact(update, context):
+    emo = get_user_emo(context.user_data)
+    print(update.message.contact)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Готово {emo}')
+
+def get_location(update, context):
+    emo = get_user_emo(context.user_data)
+    print(update.message.location)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Готово {emo}')
+
 
 
 def main():
@@ -26,9 +69,13 @@ def main():
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler('cat', send_cat_picture, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('^(Прислать котика)$'), send_cat_picture, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('^(Сменить аватар)$'), chenge_avatar, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling() # ходи в телегу слушай сообщения
     mybot.idle # Работай пока не остоновят принудительно
